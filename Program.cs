e5051ee36e7de143fb3908c9e42c327830a35e0a
Thauezetes <50925608+Thauezetes@users.noreply.github.com>
2019-05-22 15:22:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Numdig, M3, M5, MM;
+            int i;
+            Console.Write("Digite um número: ");
+            Numdig = Convert.ToDouble(Console.ReadLine());
+
+
+            for (i = 1; i <= Numdig; i++)
+            {
+
+                M3 = i % 3;
+                M5 = i % 5;
+                MM = M3 + M5;
+
+                if (MM == 0)
+                {
+                    Console.WriteLine("BizzBuzz");
+
+                }
+                else if (M3 == 0)
+                {
+                    Console.WriteLine("Bizz");
+                }
+                else if (M5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
